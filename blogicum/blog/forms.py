8e58b662254be14675784691f705f417b871a575
Nastya Platonova <nastya27platonova@gmail.com>
2024-12-24 00:48:00 +0300

    исправила ошибки после проверки

--- blogicum/blog/forms.py
+++ blogicum/blog/forms.py
@@ -39,3 +39,4 @@ class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ('text',)
+ 
--- blogicum/pages/views.py
+++ blogicum/pages/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render  # type: ignore[import-untyped]
-#from django.http import HttpResponse  # type: ignore[import-untyped]
+# from django.http import HttpResponse  # type: ignore[import-untyped]
 from django.views.generic import TemplateView
 
 
