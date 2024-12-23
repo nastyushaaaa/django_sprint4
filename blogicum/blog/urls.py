af2f14586d57c1369af0611d3732267dbeaef422
Nastya Platonova <nastya27platonova@gmail.com>
2024-12-24 00:40:48 +0300

    работает, сервер открывается, отправляю на тесты

--- blogicum/blog/urls.py
+++ blogicum/blog/urls.py
@@ -5,9 +5,10 @@ from . import views
 app_name: str = 'blog'
 
 urlpatterns: list[path] = [
-    path('', views.index, name='index'),
-    path('posts/<int:id>/', views.post_detail, name='post_detail'),
-    path('category/<slug:category_slug>/', views.category_posts,
+    path('', views.PostListView.as_view(), name='index'),
+    path('posts/<int:post_id>/', views.PostDetailView.as_view(),
+         name='post_detail'),
+    path('category/<slug:category_slug>/', views.PostCategoryView.as_view(),
          name='category_posts'),
     path('profile/<str:username>/', views.ProfileListView.as_view(),
          name='profile'),
